73630e7ee2ca314e3e57dac39f720453a5064061
cleoivvy <claraojobo@gmail.com>
2024-07-25 15:12:32 +0100

    fix: modify the changes

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-// background colour
+// background color
 document.body.style.backgroundColor = 'blue'
 
 // getElementById to select the 3rd list
@@ -15,18 +15,18 @@ leap.textContent ='forever'
 
 // applying background color and content to the 10th li
 const prove =  document.querySelectorAll('ol>li ')
-prove [9].style.backgroundImage = 'linear-gradient(45deg, red 30%, gray 31%, gray 66%, black 67%)'
-prove [9].textContent= 'pearl'
+prove[9].style.backgroundImage = 'linear-gradient(45deg, red 30%, gray 30%, gray 66%, black 66%)'
+prove[9].textContent= 'pearl'
 
 // getElementsByClassName to apply style to the 5th li
 const learn =document.getElementsByClassName('peer')
-learn [2].style.backgroundColor ='purple'
-learn [2].textContent ='bear'
+learn[2].style.backgroundColor ='purple'
+learn[2].textContent ='bear'
 
 // getElementsByClassName to apply style to the 7th li
 const bottle =document.getElementsByClassName('peer')
-bottle [3].style.backgroundColor ='pink'
-bottle [3].textContent ='love'
+bottle[3].style.backgroundColor ='pink'
+bottle[3].textContent ='love'
 
 // getElementByTagName give content to the 4th li
 const purr = document.getElementsByTagName('li')
